4daa577b74ea715dc63ec7fbe9aeffefa09d45f5
Matteo Gabriele <matteo@blocklevel.nl>
2017-02-26 23:18:34 +0100

    fix(component): add correct wrapper
    see #3

--- src/component/background/background.vue
+++ src/component/background/background.vue
@@ -1,17 +1,19 @@
 <script src="./background.js"></script>
 <style module src="./background.css"></style>
 <template>
-  <div :class="$style.component" :style="wrapperStyle">
-    <transition
-      :enter-class="$style.enter"
-      :enter-active-class="$style.before">
-      <div v-if="shouldImageRender" :class="$style.image" :style="imageStyle"></div>
-    </transition>
+  <div :class="$style.component">
+    <div :style="wrapperStyle">
+      <transition
+        :enter-class="$style.enter"
+        :enter-active-class="$style.before">
+        <div v-if="shouldImageRender" :class="$style.image" :style="imageStyle"></div>
+      </transition>
 
-    <transition
-      :enter-class="$style.enter"
-      :enter-active-class="$style.before">
-      <div v-if="shouldPlaceholderRender" :class="$style.placeholder" :style="placeholderStyle"></div>
-    </transition>
+      <transition
+        :enter-class="$style.enter"
+        :enter-active-class="$style.before">
+        <div v-if="shouldPlaceholderRender" :class="$style.placeholder" :style="placeholderStyle"></div>
+      </transition>
+    </div>
   </div>
 </template>
--- src/component/image/image.vue
+++ src/component/image/image.vue
@@ -1,25 +1,27 @@
 <script src="./image.js"></script>
 <style module src="./image.css"></style>
 <template>
-  <div :class="$style.component" :style="wrapperStyle">
-    <transition
-      :enter-class="$style.enter"
-      :enter-active-class="$style.before">
-      <img
-        v-if="shouldImageRender"
-        :class="$style.image"
-        :src="image"
-      />
-    </transition>
-    <transition
-      :enter-class="$style.enter"
-      :enter-active-class="$style.before">
-      <img
-        v-if="shouldPlaceholderRender"
-        :class="shouldImageRender ? $style.out : $style.placeholder"
-        :style="blurStyle"
-        :src="placeholderImage"
-      />
-    </transition>
+  <div :class="$style.component">
+    <div :style="wrapperStyle">
+      <transition
+        :enter-class="$style.enter"
+        :enter-active-class="$style.before">
+        <img
+          v-if="shouldImageRender"
+          :class="$style.image"
+          :src="image"
+        />
+      </transition>
+      <transition
+        :enter-class="$style.enter"
+        :enter-active-class="$style.before">
+        <img
+          v-if="shouldPlaceholderRender"
+          :class="shouldImageRender ? $style.out : $style.placeholder"
+          :style="blurStyle"
+          :src="placeholderImage"
+        />
+      </transition>
+    </div>
   </div>
 </template>
